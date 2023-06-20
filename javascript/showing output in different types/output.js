c130d95400f1c55de6f1dc0d36bd1584bdb744f8
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2023-06-20 21:59:41 +0530

    javascript introduction

---
+++ javascript/showing output in different types/output.js
@@ -0,0 +1,9 @@
+document.getElementById("title").innerHTML = "Learning different ways to show the output using js"
+
+console.log("good evening,hii")
+
+alert("good morningasdfasdf")
+
+document.write("write will remove existing html and adds the content that was passed in write")
+
+window.print()
---
+++ javascript/variables in js/variable.js
@@ -0,0 +1,32 @@
+// variables 
+
+// variables are used to store information in the format like String, number, boolean, arrays, objects, undefined, null
+
+// varibles names should always starts with letters or ( _ ) or ( $ )
+// variables names shouldn't starts with number 
+
+
+
+
+// favouriteFood - valid
+// $laptopName -valid
+// _carName -valid
+
+// 5thclass - not valid
+
+// class5 -valid
+
+myName = "shiva kumar"
+console.log(myName)
+
+$phoneNumber = 898986565
+console.log($phoneNumber)
+
+_isStudent = false;
+console.log(_isStudent)
+
+_isEmployed = true;
+console.log(_isEmployed)
+
+
+// var, let and const 
